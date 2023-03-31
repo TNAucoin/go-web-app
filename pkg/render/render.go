6fb98563a8eac38247509ac537988a433ab88618
Travis Aucoin <travisdevaucoin@gmail.com>
2023-03-30 22:01:16 -0500

    render template refactor

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,7 +8,7 @@ import (
 
 // TemplateRenderer parses the template file and Executes it using the ResponseWriter
 func TemplateRenderer(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template: ", err)
